29f4445e1c832aaf7b7865ad6d5e2f8fac1830ba
xenia <ksen.novikova@mail.ru>
2022-10-17 23:13:22 +0300

    комит сделан

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String firstName = " Семён";
+        String middleName = " Семёнович";
+        String lastName = " Иванов";
+        String fullName = firstName + middleName + lastName;
+        System.out.println("ФИО сотрудника — " + fullName);
+        String fullNameToUpperCase=fullName.toUpperCase();
+        System.out.println("ФИО сотрудника для отчета — " + fullNameToUpperCase);
+        String fullNameE=fullName.replace('ё','е');
+        System.out.println("ФИО сотрудника для отчета без ё"+fullNameE);
+
+
     }
 }
